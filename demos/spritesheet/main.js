e6fec180b6e70f499a871d788a000da542a10145
dustinpfister <dustin.pfister@gmail.com>
2021-09-12 12:58:19 -0400

    have a draw cells method that works good so far

--- demos/spritesheet/main.js
+++ demos/spritesheet/main.js
@@ -13,24 +13,29 @@ var sm = gameFrame.smCreateMain({
     }
 });
 
+var drawCell = function(stack, layerIndex, sheetKey, cellIndex, disp){
+    var layerObj = stack[layerIndex],
+    canvas = layerObj.canvas,
+    ctx = layerObj.ctx,
+    spriteSheet = stack.spriteSheets[sheetKey],
+    cellInfo = spriteSheet.cells[cellIndex];
+    // draw the cell
+    ctx.drawImage(spriteSheet.image, cellInfo.x, cellInfo.y, cellInfo.w, cellInfo.h, disp.x, disp.y, disp.w, disp.h);
+};
+
 // a game state
 gameFrame.smPushState(sm, {
     name: 'game',
-    buttons: {
-        //back: { x: 100, y: 100, w: 64, h:64, disp: 'New Game', onClick: function(e, pos, sm, button){      
-        //}}
-    },
+    buttons: {},
     start: function(sm){
         // drawing background once
         canvasMod.draw(sm.layers, 'background', 0);
-
         // creating a sprite sheet object with an array of objects like this
         var cellIndex = [
             { x: 0, y:0, w: 32, h: 32 },
             { x: 32, y:0, w: 32, h: 32 }
         ];
         canvasMod.createSpriteSheet(sm.layers, 'sheet-test1', 0, cellIndex);
-
         // creating a sprite sheet object with a function that will return
         // an array of objects like above
         var cellIndexFunction = function(image, spriteSheet, stack){
@@ -44,14 +49,10 @@ gameFrame.smPushState(sm, {
             return cells;
         };
         canvasMod.createSpriteSheet(sm.layers, 'sheet-test2', 0, cellIndexFunction);
-
-
-console.log(sm.layers.spriteSheets);
-
+        // sheets look good
+        console.log(sm.layers.spriteSheets);
     },
-    update: function(sm, secs){
-
-    },
+    update: function(sm, secs){},
     draw: function(sm, layers){
         var canvas = layers[1].canvas,
         ctx = layers[1].ctx;
@@ -59,8 +60,11 @@ console.log(sm.layers.spriteSheets);
         canvasMod.draw(layers, 'print', 1, sm.currentState, 10, 10);
         canvasMod.draw(layers, 'stateButtons', 1, sm);
         // drawing images to the canvas
-        ctx.drawImage(sm.layers.images[0], 0, 0, 32, 32, 200, 200, 64, 64);
-        ctx.drawImage(sm.layers.images[1], 100.5, 29.5);
+
+        drawCell(layers, 1, 'sheet-test1', 0, {x: 350, y: 150, w: 64, h: 64})
+
+        //ctx.drawImage(sm.layers.images[0], 0, 0, 32, 32, 200, 200, 64, 64);
+        //ctx.drawImage(sm.layers.images[1], 100.5, 29.5);
     },
     events: {
         pointerStart: function(e, pos, sm){},
