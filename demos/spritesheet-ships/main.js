04ace40195ee5c50b91dfd337996c29e02d1fb7b
dustinpfister <dustin.pfister@gmail.com>
2021-09-14 15:19:18 -0400

    clean up

--- demos/spritesheet-ships/main.js
+++ demos/spritesheet-ships/main.js
@@ -13,7 +13,7 @@ var sm = gameFrame.smCreateMain({
     }
 });
 
-
+// set up the ships pool
 sm.game.ships = poolMod.create({
     count: 8,
     secsCap: 0.25,
@@ -26,39 +26,29 @@ sm.game.ships = poolMod.create({
         obj.data.image = sm.layers.images[0];
         obj.data.cellIndex = 0;
         obj.data.cellSecs = 0;
-
+        // other values
         obj.data.homeRadian = Math.PI * 2 / pool.objects.length * obj.i;
         obj.data.deltaRadian = 0;
         obj.data.radian = obj.data.homeRadian;
         obj.data.radius = 200;
     },
     update: function (obj, pool, sm, secs){
+        // update heading
+        obj.heading = Math.PI + Math.atan2(240 - (obj.y + 32), 320 - (obj.x + 32));
+        var fast = true;
+        // SETTING CELL INDEX BASED ON HEADING AND SPEED
+        var dir = Math.round( obj.heading / (Math.PI * 2) * 8 );
+        dir = dir >= 8 ? 7 : dir;
+        obj.data.cellIndex = 2 * dir + (fast ? 1: 0);
         // USING OLD IMGD METHOD FOR NOW
-       //obj.data.cellSecs += secs;
-/*
-       if(obj.data.cellSecs > 0.1){
-           obj.data.cellIndex += 1;
-           obj.data.cellIndex %= 4;
-           obj.data.cellSecs = 0;
-       }
-*/
+        obj.data.imgD = sm.layers.spriteSheets['ship-type-one'].cells[obj.data.cellIndex];
 
-       obj.heading = Math.PI + Math.atan2(240 - (obj.y + 32), 320 - (obj.x + 32));
-
-       var fast = true,
-       dir = Math.round( obj.heading / (Math.PI * 2) * 8 );
-       dir = dir >= 8 ? 7 : dir;
-       obj.data.cellIndex = 2 * dir + (fast ? 1: 0);
-
-
-       obj.data.imgD = sm.layers.spriteSheets['ship-type-one'].cells[obj.data.cellIndex];
-
-       obj.data.deltaRadian = Math.PI / 180 * 45 * secs;
-       obj.data.radian += obj.data.deltaRadian;
-       obj.data.radian = utils.mod(obj.data.radian, Math.PI * 2);  
-       obj.lifespan = 1;
-       obj.x = 320 - obj.w / 2 + Math.cos(obj.data.radian) * obj.data.radius;
-       obj.y = 240 - obj.h / 2 + Math.sin(obj.data.radian) * obj.data.radius;
+        obj.data.deltaRadian = Math.PI / 180 * 45 * secs;
+        obj.data.radian += obj.data.deltaRadian;
+        obj.data.radian = utils.mod(obj.data.radian, Math.PI * 2);  
+        obj.lifespan = 1;
+        obj.x = 320 - obj.w / 2 + Math.cos(obj.data.radian) * obj.data.radius;
+        obj.y = 240 - obj.h / 2 + Math.sin(obj.data.radian) * obj.data.radius;
     }
 });
 
@@ -68,33 +58,17 @@ gameFrame.smPushState(sm, {
     name: 'game',
     buttons: {},
     start: function(sm){
-
-        // drawing background once
         canvasMod.draw(sm.layers, 'background', 0);
-
-
-
         canvasMod.createSpriteSheetGrid(sm.layers, 'ship-type-one', 0, 32, 32);
-
-        // sheets look good
-        console.log(sm.layers.spriteSheets);
-
         poolMod.spawnAll(sm.game.ships, sm, {});
-
     },
     update: function(sm, secs){
-
         poolMod.update(sm.game.ships, secs, sm);
     },
     draw: function(sm, layers){
-
         canvasMod.draw(layers, 'clear', 1);
-        canvasMod.draw(layers, 'print', 1, sm.currentState, 10, 10);
         canvasMod.draw(layers, 'stateButtons', 1, sm);
-
-
         canvasMod.draw(layers, 'pool', 1, sm.game.ships);
-
     },
     events: {
         pointerStart: function(e, pos, sm){},
