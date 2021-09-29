6eb8a15d43c89563deb584d14d66b616be7041fe
dustinpfister <dustin.pfister@gmail.com>
2021-09-29 08:11:53 -0400

    print line helper

--- demos/chicken-cooker-weapons/js/cc-canvas-plug.js
+++ demos/chicken-cooker-weapons/js/cc-canvas-plug.js
@@ -9,19 +9,19 @@ Clucker.canvasMod.load({
                     fillStyle: 'rgba(255, 255, 0, 0.5)'
                 });
                 // draw hp bars
-                Clucker.poolMod.getActiveObjects(sm.game.chickens).forEach(function(chk){
-                    if(chk.data.state != 'cooked' && chk.data.stat.hp != chk.data.stat.hpMax){
+                Clucker.poolMod.getActiveObjects(sm.game.chickens).forEach(function (chk) {
+                    if (chk.data.state != 'cooked' && chk.data.stat.hp != chk.data.stat.hpMax) {
                         ctx.fillStyle = 'gray';
                         ctx.fillRect(chk.x, chk.y, 20, 5);
                         ctx.fillStyle = 'lime';
                         var hpPer = chk.data.stat.hp / chk.data.stat.hpMax;
-                        if(hpPer <= 0.5){
+                        if (hpPer <= 0.5) {
                             ctx.fillStyle = 'orange';
                         }
-                        if(hpPer <= 0.25){
+                        if (hpPer <= 0.25) {
                             ctx.fillStyle = 'red';
                         }
-                        ctx.fillRect(chk.x, chk.y, 20 * ( chk.data.stat.hp / chk.data.stat.hpMax  ), 5);
+                        ctx.fillRect(chk.x, chk.y, 20 * (chk.data.stat.hp / chk.data.stat.hpMax), 5);
                     }
                 });
             }
@@ -49,22 +49,21 @@ Clucker.canvasMod.load({
                 ctx.fillStyle = 'blue';
                 ctx.fillRect(x, 35, w * per3, 5);
             }
-        },
-        {
+        }, {
             name: 'fun-facts-guy',
             method: function (stack, ctx, canvas, layerObj, sm) {
                 var talk = sm.funFacts.disp.talk;
                 Clucker.canvasMod.draw(sm.layers, 'sprite', 1, sm.funFacts.disp.base, {});
                 Clucker.canvasMod.draw(sm.layers, 'sprite', 1, sm.funFacts.disp.hair, {});
                 Clucker.canvasMod.draw(sm.layers, 'sprite', 1, sm.funFacts.disp.mouth, {});
-                Clucker.canvasMod.draw(sm.layers, 'sprite', 1, sm.funFacts.disp.hand, {}); 
+                Clucker.canvasMod.draw(sm.layers, 'sprite', 1, sm.funFacts.disp.hand, {});
                 Clucker.canvasMod.draw(sm.layers, 'sprite', 1, talk, {});
                 var lines = sm.funFacts.lines,
                 len = lines.length,
                 fontSize = 12,
                 x = talk.x + talk.w / 2,
                 y = talk.y + talk.h / 2 - fontSize * len / 2 - fontSize / 2;
-                lines.forEach(function(line){
+                lines.forEach(function (line) {
                     y += fontSize;
                     Clucker.canvasMod.draw(sm.layers, 'print', 1, line, x, y, {
                         align: 'center',
@@ -82,15 +81,22 @@ Clucker.canvasMod.load({
                 var printOptions = {
                     fontSize: 15
                 };
-                Clucker.canvasMod.draw(stack, 'print', 1, 'score : ' + sm.game.score, 10, 10, printOptions);
-                Clucker.canvasMod.draw(stack, 'print', 1, 'cpm avg : ' + sm.game.cpm.avg, 10, 30, printOptions);
-                var spawn = sm.game.spawn;
-                Clucker.canvasMod.draw(stack, 'print', 1, 'active: ' + spawn.activeCount + '/' + spawn.currentMaxActive, 10, 50, printOptions);
-                Clucker.canvasMod.draw(stack, 'print', 1, 'spawn rate: ' + spawn.rate.toFixed(2), 10, 70, printOptions);
+                // print line helper
+                var printLine = function (mess, yIndex) {
+                    Clucker.canvasMod.draw(stack, 'print', 1, mess, 10, 10 + 20 * yIndex, printOptions);
+                };
+                printLine('score: ' + sm.game.score, 0);
+                printLine('cpm avg: ' + sm.game.cpm.avg, 1);
+
+                //Clucker.canvasMod.draw(stack, 'print', 1, 'score : ' + sm.game.score, 10, 10, printOptions);
+                //Clucker.canvasMod.draw(stack, 'print', 1, 'cpm avg : ' + sm.game.cpm.avg, 10, 30, printOptions);
+                //var spawn = sm.game.spawn;
+                //Clucker.canvasMod.draw(stack, 'print', 1, 'active: ' + spawn.activeCount + '/' + spawn.currentMaxActive, 10, 50, printOptions);
+                //Clucker.canvasMod.draw(stack, 'print', 1, 'spawn rate: ' + spawn.rate.toFixed(2), 10, 70, printOptions);
                 // fun facts info
-                Clucker.canvasMod.draw(stack, 'print', 1, 'ff idle secs: ' + sm.funFacts.idleSecs.toFixed(2), 10, 90, printOptions);
-                Clucker.canvasMod.draw(stack, 'print', 1, 'ff talk secs: ' + sm.funFacts.talkSecs.toFixed(2), 10, 110, printOptions);
-		
+                //Clucker.canvasMod.draw(stack, 'print', 1, 'ff idle secs: ' + sm.funFacts.idleSecs.toFixed(2), 10, 90, printOptions);
+                //Clucker.canvasMod.draw(stack, 'print', 1, 'ff talk secs: ' + sm.funFacts.talkSecs.toFixed(2), 10, 110, printOptions);
+
             }
         }
     ]
