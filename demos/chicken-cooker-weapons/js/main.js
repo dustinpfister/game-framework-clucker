082b142ef8c979f9404e7ea931fda04f8bd26b59
dustinpfister <dustin.pfister@gmail.com>
2021-09-30 14:01:16 -0400

    creating the game object in the init state

--- demos/chicken-cooker-weapons/js/main.js
+++ demos/chicken-cooker-weapons/js/main.js
@@ -31,7 +31,7 @@ sm.CHICKENS_PPS_MAX = 256; // max pixels per second for chicken speed
 sm.CHICKENS_SIZE = 64; // the scaled size of chickens
 sm.CHICKENS_CELL_SIZE = 32; // this will need to be adjusted when using a higher res sprite sheet
 
-sm.game = gameMod.create({}, sm);
+
 
 // HELPERS
 
@@ -68,6 +68,8 @@ var createToGameButton = function () {
 Clucker.gameFrame.smPushState(sm, {
     name: 'init',
     start: function (sm, canvasMod) {
+        // create new game object
+        sm.game = gameMod.create({}, sm);
         // set button desc for first time
         sm.states.gameTime.buttons.weapon.desc = sm.game.currentWeapon;
         // create sm.funFacts
