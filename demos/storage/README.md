72a10f1f6ef308767bcd4101f4dae7e3d8831d7a
dustinpfister <dustin.pfister@gmail.com>
2021-10-23 17:46:55 -0400

    started a new storage demo based off of the hello world demo

---
+++ demos/storage/README.md
@@ -0,0 +1,4 @@
+# storage demo
+
+As of Clucker 0.7.0 I now have a storage module that thus far makes use of localStoarge only, but might it time also support other options for saving a game state if I put more time into this specifc module of Clucker. Anyway whenever I add I new major feature to Clucker I often make at least one demo that is a way to just test out that the feature of Clucker is working as it should and this is such a demo this time with the storage module of Clucker.
+
---
+++ demos/storage/main.js
@@ -0,0 +1,106 @@
+// to create an object pool with default hard coded settings
+//var pool = poolMod.create();
+//console.log( pool.objects.length ) // 10
+//console.log( JSON.stringify( pool.objects[0] ) );
+// {"active":false,"i":0,"x":0,"y":0,"w":32,"h":32,"heading":0,"pps":32,"lifespan":3,"data":{}}
+
+
+console.log('Using clucker v' +this['Clucker'].ver)
+
+
+// create an sm object
+//var sm = Clucker.gameFrame.smCreateMain({
+var sm = Clucker.createMain({
+    currentState: 'game', // set starting state object to use
+    width: 640,
+    height: 480,
+    game: {
+        text: 'Hello World',
+        pool: Clucker.poolMod.create({
+            count: 8,
+            secsCap: 0.25,
+            disableLifespan: true,
+            spawn: function(obj, pool){
+                obj.data.homeRadian = Math.PI * 2 / pool.objects.length * obj.i;
+                obj.data.deltaRadian = 0;
+                obj.data.radian = obj.data.homeRadian;
+                obj.data.radius = 200;
+            },
+            update: function (obj, pool, sm, secs){
+               obj.data.deltaRadian = Math.PI / 180 * 45 * secs;
+               obj.data.radian += obj.data.deltaRadian;
+               obj.data.radian = Clucker.utils.mod(obj.data.radian, Math.PI * 2);  
+               obj.lifespan = 1;
+               obj.x = 320 - obj.w / 2 + Math.cos(obj.data.radian) * obj.data.radius;
+               obj.y = 240 - obj.h / 2 + Math.sin(obj.data.radian) * obj.data.radius;
+            }
+        }),
+        cx: 320,
+        cy: 240,
+        x: 0,
+        y: 0,
+        dir: 1,
+        dx: 0,
+        printOptions: {
+            align: 'center',
+            baseLine: 'middle',
+            fontSize: 40
+        },
+        pointerDown: false
+    }
+});
+
+// add at least one state object
+Clucker.pushState(sm, {
+    name: 'game',
+    // start hook will just fire once when the state object starts
+    start: function(sm, canvasMod){
+        // draw background once
+        canvasMod.draw(sm.layers, 'background', 0);
+
+        // spawn
+        Clucker.poolMod.spawnAll(sm.game.pool, sm, {});
+    },
+    // what to do on each update
+    update: function(sm, secs){
+        sm.game.dx += 64 * secs * sm.game.dir;
+        if(sm.game.dx >= 32){
+            sm.game.dx = 32;
+            sm.game.dir = -1;
+        }
+        if(sm.game.dx <= -32){
+            sm.game.dx = -32;
+            sm.game.dir = 1;
+        }
+        sm.game.x = sm.game.cx + sm.game.dx;
+        sm.game.y = sm.game.cy;
+        // update game.pool
+        Clucker.poolMod.update(sm.game.pool, secs, sm);
+    },
+    // draw will be called after each update
+    draw: function(sm, layers, canvasMod){
+        canvasMod.draw(layers, 'clear', 1);
+        canvasMod.draw(layers, 'pool', 1, sm.game.pool);
+        canvasMod.draw(layers, 'print', 1, sm.game.text, sm.game.x, sm.game.y, sm.game.printOptions);
+    },
+    // events for this state
+    events: {
+        pointerStart: function(e, pos, sm){
+            sm.game.pointerDown = true;
+        },
+        pointerMove: function(e, pos, sm){
+            if(sm.game.pointerDown){
+                sm.game.cx = pos.x;
+                sm.game.cy = pos.y;
+            }
+        },
+        pointerEnd: function(e, pos, sm){
+            sm.game.pointerDown = false;
+            sm.game.cx = 320;
+            sm.game.cy = 240;
+        }
+    }
+});
+// start the state machine
+Clucker.setState(sm, 'game');
+sm.loop();
