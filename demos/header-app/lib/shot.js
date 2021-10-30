e2ce7906cd566c1288ba0407c4bcfb9077ba569b
dustinpfister <dustin.pfister@gmail.com>
2021-10-30 15:05:38 -0400

    by default using method2

--- demos/header-app/lib/shot.js
+++ demos/header-app/lib/shot.js
@@ -17,25 +17,37 @@
     };
 
     // THE GET SHOOT AT ANGLE HELPER
-    var method2 = function(unit, target){
-        var a1 = getTargetAngle(unit, target),
-        // adjust secsStandard based on target speed
-        secsStandard = 1, //1 - 0.85 * (target.pps / SHIP_PPS[SHIP_PPS.length - 1] ),
-        futureTarget1 = createFutureTarget(target, secsStandard),
-        d1 = Clucker.utils.distance(unit.x, unit.y, futureTarget1.x, futureTarget1.y), // distance to future target
-        secsShot = d1 / unit.stat.shotPPS,
-        //secsForHit = secsShot / secsStandard,
-        futureTarget2 = createFutureTarget(target, secsShot),
-        a2 = getTargetAngle(unit, futureTarget2);
-        return a2;
+    var getShootAtMethods = {
+        // method1 is just basicly using Math.atan2 with adjusted values
+        method1 : function(unit, target){
+            return getTargetAngle(unit, target);
+        },
+        // method2 involves creating a future target and shooting as that
+        // seems to work okay, most of the time but is far from perfect it will
+        // be way off for certian values when it comes to playing around with shot
+        // and ship PPS values
+        method2 : function(unit, target){
+            var a1 = getTargetAngle(unit, target),
+            // adjust secsStandard based on target speed
+            secsStandard = 1, //1 - 0.85 * (target.pps / SHIP_PPS[SHIP_PPS.length - 1] ),
+            futureTarget1 = createFutureTarget(target, secsStandard),
+            d1 = Clucker.utils.distance(unit.x, unit.y, futureTarget1.x, futureTarget1.y), // distance to future target
+            secsShot = d1 / unit.stat.shotPPS,
+            //secsForHit = secsShot / secsStandard,
+            futureTarget2 = createFutureTarget(target, secsShot),
+            a2 = getTargetAngle(unit, futureTarget2);
+            return a2;
+        }
     };
 
 /********* ********** **********
   PUBLIC METHODS
 ********** ********** *********/
 
-    shotMod.getShootAtAngle = function(unit, target){
-         return method2(unit, target); //getTargetAngle(unit, target);
+    shotMod.getShootAtAngle = function(unit, target, methodName){
+         methodName = methodName || 'method2';
+         var method = getShootAtMethods[methodName];
+         return method(unit, target);
     };
 
 
