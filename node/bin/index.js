166fe25cfbd54732877016cf277de49e27655469
dustinpfister <dustin.pfister@gmail.com>
2021-11-27 13:46:08 -0500

    basic index of main index.js file for bin folder working

--- node/bin/index.js
+++ node/bin/index.js
@@ -1,2 +1,13 @@
-var argv = process.argv.slice(2, process.argv.length);
-console.log(argv);
+const path = require('path');
+// argv array
+let argv = process.argv.slice(2, process.argv.length);
+// get the sub command string
+let subCommand = argv[0] === undefined ? 'default' : argv[0];
+// try to load sub command module
+try{
+    let mod = require( path.join(__dirname, subCommand, 'index.js') );
+    console.log(mod);
+}catch(e){
+   console.warn('Error trying to use sub command \"' + subCommand + '\"');
+   console.warn(e.message);
+}
